e0d99dd8381b8387242a79e347bcc05122eaf808
Pat Rao <pratz.rao@gmail.com>
2018-04-22 21:16:47 -0500

    First commit

---
+++ src/main/java/io/javabrains/springbootstarter/Test_Main.java
@@ -0,0 +1,13 @@
+package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test_Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test_Main.class, args);
+	}
+
+}
